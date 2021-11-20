dac7e6604d29638a75dff9e793491aae2fa25a6e
ishaban <ishaban.flash@gmail.com>
2021-11-20 16:49:31 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express, {
+    Request as ExRequest,
+    Response as ExResponse,
+} from 'express'
+import bodyParser from 'body-parser'
+import {RegisterRoutes} from '../build/routes'
+import swaggerUi from 'swagger-ui-express'
+
+export const app = express()
+
+// Use body parser to read sent json payloads
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    }),
+)
+app.use(bodyParser.json())
+
+app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+  return res.send(
+    swaggerUi.generateHTML(await import("../build/swagger.json"))
+  );
+});
+
+RegisterRoutes(app)
